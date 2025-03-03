53f339bf62cc1438ce410f98b60d9f92c4ff93de
Hannah Hironaka <hmmh@google.com>
2025-03-02 22:52:15 -0500

    Update copy

--- src/components/Home/Cook/index.tsx
+++ src/components/Home/Cook/index.tsx
@@ -17,11 +17,11 @@ const Cook = () => {
                     <div className='col-span-6 flex flex-col justify-center'>
                         <p className='text-primary text-lg font-normal mb-3 tracking-widest uppercase text-start'>cook with us</p>
                         <h2 className="text-3xl lg:text-5xl font-semibold text-black dark:text-white text-start">
-                            Cooking together with the expert.
+                        Recipes without the reading.
                         </h2>
-                        <p className='text-black/50 dark:text-white/50 md:text-lg font-normal mb-10 text-start mt-2'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem </p>
-                        <p className='text-black/50 dark:text-white/50 md:text-lg font-normal mb-10 text-start mt-1'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium....</p>
-                        <button className='text-xl font-medium rounded-full text-white py-5 px-6 bg-primary lg:px-10 mr-6 w-fit border border-primary hover:bg-transparent hover:text-primary'>Learn more</button>
+                        <p className='text-black/50 dark:text-white/50 md:text-lg font-normal mb-10 text-start mt-2'>No more squinting at screens with messy hands! Our Chrome extension turns any online recipe into a hands-free, voice-guided experience. Just install, select your recipe, and let AI walk you through step by step—so you can focus on cooking, not reading.</p>
+                        <p className='text-black/50 dark:text-white/50 md:text-lg font-normal mb-10 text-start mt-1'>Need to pause? Just ask. Wondering “What’s the next step?” or “What can I substitute for this ingredient?”—our AI has the answers. Whether you're chopping, stirring, or adjusting on the fly, your voice assistant is there to help.</p>
+                        <button className='text-xl font-medium rounded-full text-white py-5 px-6 bg-primary lg:px-10 mr-6 w-fit border border-primary hover:bg-transparent hover:text-primary'>Install now</button>
                     </div>
                 </div>
             </div>
