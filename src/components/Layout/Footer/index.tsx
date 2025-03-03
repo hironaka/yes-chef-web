fa4dd5dfcc8b3df7b8d20aa51f880e4d51215009
Hannah Hironaka <hmmh@google.com>
2025-03-02 19:03:06 -0500

    Fix the title of the page

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -8,41 +8,8 @@ const Footer: FC = () => {
   return (
     <footer className="pt-16 bg-darkmode">
       <div className="container mx-auto lg:max-w-screen-xl md:max-w-screen-md px-4">
-        <div className="grid grid-cols-1 sm:grid-cols-5 lg:gap-20 md:gap-6 sm:gap-12 gap-6  pb-16">
-          <div className="col-span-2">
-            <Logo />
-            <p className="text-xs font-medium text-grey dark:text-white/50 mt-5 mb-16 max-w-70%">
-              Open an account in minutes, get full financial
-              control for much longer.
-            </p>
-            <div className="flex gap-6 items-center">
-              <Link href="#" className="group bg-white hover:bg-primary rounded-full shadow-xl p-3">
-                <Icon
-                  icon="fa6-brands:facebook-f"
-                  width="16"
-                  height="16"
-                  className=" group-hover:text-white text-black"
-                />
-              </Link>
-              <Link href="#" className="group bg-white hover:bg-primary rounded-full shadow-xl p-3">
-                <Icon
-                  icon="fa6-brands:instagram"
-                  width="16"
-                  height="16"
-                  className=" group-hover:text-white text-black"
-                />
-              </Link>
-              <Link href="#" className="group bg-white hover:bg-primary rounded-full shadow-xl p-3">
-                <Icon
-                  icon="fa6-brands:x-twitter"
-                  width="16"
-                  height="16"
-                  className=" group-hover:text-white text-black"
-                />
-              </Link>
-            </div>
-          </div>
-          <div className="">
+        <div className="grid grid-cols-1 sm:grid-cols-5 lg:gap-20 md:gap-6 sm:gap-12 gap-6  pb-16 justify-center">
+          <div className="col-start-2">
             <h4 className="text-black dark:text-white mb-9 font-semibold text-xl">Company</h4>
             <ul>
               <li className="pb-5">
