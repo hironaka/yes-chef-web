d08f17f4dbee6d727559e106452048fbf5ed04a8
Hannah Hironaka <hmmh@google.com>
2025-04-06 16:46:54 -0400

    Updated home buttons

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -14,9 +14,9 @@ const Hero = () => {
                         Turn recipes into conversations
                         </h1>
                         <p className='text-black/55 dark:text-white/50 lg:text-lg font-normal mb-10 lg:text-start text-center'>Convert any online recipe into a voice-guided experience. No more messy screens—just smooth cooking.</p>
-                        <div className='md:flex align-middle justify-center lg:justify-start'>
-                            <Link href='#about-section' className='text-xl w-full md:w-auto font-medium rounded-full text-white py-5 px-6 bg-primary hover:text-primary lg:px-14 mr-6 border border-primary hover:bg-transparent'>Explore now</Link>
-                            <Link href='#cook-section' className='flex border w-full md:w-auto mt-5 md:mt-0 border-primary justify-center rounded-full text-xl font-medium items-center py-5 px-10 text-primary hover:text-white hover:bg-primary'>Let's cook</Link>
+                        <div className='flex gap-4 items-center justify-center lg:justify-start'>
+                            <Link href='#about-section' className='text-xl flex-1 font-medium rounded-full text-white py-5 px-6 bg-primary hover:text-primary lg:px-14 border border-primary hover:bg-transparent text-center'>Add to Chrome</Link>
+                            <Link href='#cook-section' className='flex border flex-1 border-primary justify-center rounded-full text-xl font-medium items-center py-5 px-10 text-primary hover:text-white hover:bg-primary'>Upgrade to Pro</Link>
                         </div>
                     </div>
                     <div className='col-span-6 flex justify-center relative'>
