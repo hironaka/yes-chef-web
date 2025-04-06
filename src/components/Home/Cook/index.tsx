93fd504ccca378a92540ad6281668e96b271f4c3
Hannah Hironaka <hmmh@google.com>
2025-04-06 17:06:48 -0400

    Updated alignment for center text

--- src/components/Home/Cook/index.tsx
+++ src/components/Home/Cook/index.tsx
@@ -15,12 +15,12 @@ const Cook = () => {
                         <Image src="/images/Cook/cook.png" alt="nothing" width={636} height={808} />
                     </div>
                     <div className='col-span-6 flex flex-col justify-center'>
-                        <p className='text-primary text-lg font-normal mb-3 tracking-widest uppercase text-start'>cook with us</p>
-                        <h2 className="text-3xl lg:text-5xl font-semibold text-black dark:text-white text-start">
+                        <p className='text-primary text-lg font-normal mb-3 tracking-widest uppercase text-center lg:text-left'>cook with us</p>
+                        <h2 className="text-3xl lg:text-5xl font-semibold text-black dark:text-white text-center lg:text-left">
                         Recipes without the reading.
                         </h2>
-                        <p className='text-black/50 dark:text-white/50 md:text-lg font-normal mb-10 text-start mt-6'>No more squinting at screens with messy hands! Our Chrome extension turns any online recipe into a hands-free, voice-guided experience. Just install, select your recipe, and let AI walk you through step by step—so you can focus on cooking, not reading.</p>
-                        <p className='text-black/50 dark:text-white/50 md:text-lg font-normal mb-10 text-start'>Need to pause? Just ask. Wondering “What’s the next step?” or “What can I substitute for this ingredient?”—our AI has the answers. Whether you're chopping, stirring, or adjusting on the fly, your voice assistant is there to help.</p>
+                        <p className='text-black/50 dark:text-white/50 md:text-lg font-normal mb-10 text-left mt-6'>No more squinting at screens with messy hands! Our Chrome extension turns any online recipe into a hands-free, voice-guided experience. Just install, select your recipe, and let AI walk you through step by step—so you can focus on cooking, not reading.</p>
+                        <p className='text-black/50 dark:text-white/50 md:text-lg font-normal mb-10 text-left'>Need to pause? Just ask. Wondering “What’s the next step?” or “What can I substitute for this ingredient?”—our AI has the answers. Whether you're chopping, stirring, or adjusting on the fly, your voice assistant is there to help.</p>
                     </div>
                 </div>
             </div>
