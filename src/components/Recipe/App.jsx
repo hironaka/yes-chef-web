013699d5e9cef6949561918da37a38e969f77d02
Hannah Hironaka <hmmh@google.com>
2025-04-06 13:54:49 -0400

    More screen real estate for the recipe

--- src/components/Recipe/App.jsx
+++ src/components/Recipe/App.jsx
@@ -179,28 +179,24 @@ export default function App() {
   return (
     <>
       <Header showNav={false} />
-      <main className="absolute top-16 left-0 right-0 bottom-0">
-        <section className="absolute top-0 left-0 right-[380px] bottom-0 flex">
-          <section className="absolute top-0 left-0 right-0 bottom-32 px-4 overflow-y-auto">
-          </section>
-          <section className="absolute h-32 left-0 right-0 bottom-0 p-4">
-            <SessionControls
-              startSession={startSession}
-              stopSession={stopSession}
-              sendClientEvent={sendClientEvent}
-              sendTextMessage={sendTextMessage}
-              isSessionActive={isSessionActive}
-            />
-          </section>
-        </section>
-        <section className="absolute top-0 w-[380px] right-0 bottom-0 p-4 pt-0 overflow-y-auto">
+      <main className="flex flex-col items-center min-h-screen pt-20 px-4">
+        <div className="w-full max-w-4xl p-4">
           <RecipePanel
             recipe={recipe}
             sendClientEvent={sendClientEvent}
             sendTextMessage={sendTextMessage}
             isSessionActive={isSessionActive}
           />
-        </section>
+        </div>
+        <div className="mb-4">
+          <SessionControls
+            startSession={startSession}
+            stopSession={stopSession}
+            sendClientEvent={sendClientEvent}
+            sendTextMessage={sendTextMessage}
+            isSessionActive={isSessionActive}
+          />
+        </div>
       </main>
     </>
   );
