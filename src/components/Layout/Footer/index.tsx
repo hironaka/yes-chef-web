21ce7eb95d56f03a793e2d232c56748c10b3c23a
Hannah Hironaka <hmmh@google.com>
2025-04-20 19:27:01 -0400

    Add a privacy policy

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -15,7 +15,7 @@ const Footer: FC = () => {
           <div className="">
             <Link href="/contact" className="text-sm text-black/70 dark:text-white/70 px-5 border-r border-grey/15 dark:border-white/15 hover:text-primary dark:hover:text-primary">Contact Us</Link>
             <Link href="https://www.privacypolicies.com/live/393cca5a-d5f1-4f21-9f38-22ef85da90c9" className="text-sm text-black/70 dark:text-white/70 px-5 border-r border-grey/15 dark:border-white/15 hover:text-primary dark:hover:text-primary">Terms & Conditions</Link>
-            <Link href="#" className="text-sm text-black/70 dark:text-white/70 px-5 hover:text-primary dark:hover:text-primary">Privacy Policy</Link>
+            <Link href="https://www.privacypolicies.com/live/ad275672-77bf-440f-93ac-d85d4711e860" className="text-sm text-black/70 dark:text-white/70 px-5 hover:text-primary dark:hover:text-primary">Privacy Policy</Link>
           </div>
         </div>
       </div>
