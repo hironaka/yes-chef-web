9fafb95b87d6a087b536253e46535e4151c03dc2
Hannah Hironaka <hmmh@google.com>
2025-04-20 18:56:59 -0400

    Add back TOS and Privacy policy

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -13,7 +13,9 @@ const Footer: FC = () => {
           </p>
 
           <div className="">
-            <Link href="/contact" className="text-sm text-black/70 dark:text-white/70 px-5 hover:text-primary dark:hover:text-primary">Contact us</Link>
+            <Link href="/contact" className="text-sm text-black/70 dark:text-white/70 px-5 border-r border-grey/15 dark:border-white/15 hover:text-primary dark:hover:text-primary">Contact Us</Link>
+            <Link href="#" className="text-sm text-black/70 dark:text-white/70 px-5 border-r border-grey/15 dark:border-white/15 hover:text-primary dark:hover:text-primary">Terms & Conditions</Link>
+            <Link href="#" className="text-sm text-black/70 dark:text-white/70 px-5 hover:text-primary dark:hover:text-primary">Privacy Policy</Link>
           </div>
         </div>
       </div>
