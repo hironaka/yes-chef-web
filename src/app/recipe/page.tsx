5198454d8ee22d80c1af82c0123c526ee23a9e57
Hannah Hironaka <hmmh@google.com>
2025-07-27 21:00:20 -0400

    Remove auth guard for now

--- src/app/recipe/page.tsx
+++ src/app/recipe/page.tsx
@@ -2,14 +2,11 @@
 import React from "react";
 import App from "@/components/Recipe/App";
 import { useSession } from "next-auth/react";
-import AuthGuard from "@/components/AuthGuard";
 
 export default function Recipe() {
   return (
     <main>
-      <AuthGuard>
-        <App />
-      </AuthGuard>
+      <App />
     </main>
   );
 }
