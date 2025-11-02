b39675614db6ea3574f9d319764ea581f584854e
Hannah Hironaka <hannahhironaka@gmail.com>
2025-11-01 21:00:13 -0400

    Make the body white

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Search from "@/components/Search";
-import Gallery from "@/components/Home/Gallery";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-    title: "Search Recipes",
-};
 
 export default function SearchPage() {
+
+    useEffect(() => {
+        document.body.style.backgroundColor = "white";
+
+        return () => {
+            document.body.style.backgroundColor = "";
+        };
+    }, []);
+
     return (
         <main className="flex items-center justify-center min-h-screen bg-white">
             <Search />
